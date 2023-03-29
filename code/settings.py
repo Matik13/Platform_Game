e827423e50d7467c61ac0aee0228209cf5629ce4
Matik <matiklego@poczta.fm>
2023-03-29 15:53:36 +0200

    uploading files

---
+++ code/settings.py
@@ -0,0 +1,21 @@
+level_map = [
+'          XX                ',
+'               XX           ',
+'                  X         ',
+'                       XX   ',
+'                  XX        ',
+'            XX              ',
+'       XX                   ',
+'               XX           ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX XXXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1920
+screen_height = len(level_map) * tile_size #1080
