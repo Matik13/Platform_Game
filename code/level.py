85bf6c5162cfbbfe62821d75eb22cc0f3c1a8420
Matik <matiklego@poczta.fm>
2023-03-31 19:20:18 +0200

    added dust particles when jumping and landing

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
+from particles import ParticleEffect
 class Level:
     def __init__(self, level_data, surface):
         #setup levela
@@ -9,6 +10,29 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        #dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
+    def create_jump_particles(self,pos):
+        jump_particle_sprite = ParticleEffect(pos,'jump')
+        self.dust_sprite.add(jump_particle_sprite)
+
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
+
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(1,15)
+            else:
+                offset = pygame.math.Vector2(-1, 15)
+
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset,'land')
+            self.dust_sprite.add(fall_dust_particle)
 
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
@@ -24,7 +48,7 @@ class Level:
                 if cell == 'P':
                     x = col_index * tile_size
                     y = row_index * tile_size
-                    player_sprite = Player((x,y),self.display_surface)
+                    player_sprite = Player((x,y),self.display_surface,self.create_jump_particles)
                     self.player.add(player_sprite)
 
     def scroll_x(self):
@@ -81,12 +105,19 @@ class Level:
 
 
     def run(self):
+        #dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
+
         #level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         self.scroll_x()
+
         #player
         self.player.update()
         self.horizontal_movement_collision()
+        self.get_player_on_ground()
         self.vertical_movement_collision()
+        self.create_landing_dust()
         self.player.draw(self.display_surface)
